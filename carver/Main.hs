d13de8b93ddbfe0b711bc2886a9d80a222d60f0e
M.Magomedov <mmagomedoff@gmail.com>
2015-04-15 18:05:32 +0300

    complete example with vertical and horizontal carving

--- carver/Main.hs
+++ carver/Main.hs
@@ -1,9 +1,11 @@
 import Image.Processing.SeamCarving
 import Codec.Image.DevIL
+import Data.Array.Unboxed
 
 main = do
   ilInit
   image <- readImage "/home/mma/1.png"
-  let newImage = removeHorizontals 50 image
+  let newImage = removeVerticals 30 $ removeHorizontals 50 image
+  putStrLn $ show $ bounds newImage
   writeImage "/home/mma/output.png" newImage
   return ()
